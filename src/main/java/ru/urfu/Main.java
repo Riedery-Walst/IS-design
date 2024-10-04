17f6d81f4c83f662dfc2e7c868c5c4e4b01667e8
ClamDragonPelmeshishka <>
2024-10-04 10:18:32 +0500

    Add Lab5

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,8 +1,5 @@
 package ru.urfu;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
